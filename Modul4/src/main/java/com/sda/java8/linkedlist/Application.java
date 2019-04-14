dc3108be8699be8843ce309636a6122b4e22fbf3
Catalin Tenescu <catalin.tenescu@gmail.com>
2019-04-14 16:05:48 +0300

    Add bst

--- Modul4/src/main/java/com/sda/java8/linkedlist/Application.java
+++ Modul4/src/main/java/com/sda/java8/linkedlist/Application.java
@@ -1,35 +1,58 @@
 package com.sda.java8.linkedlist;
 
+import com.sda.java8.linkedlist.binarysearchtree.BinarySearchTree;
+
 public class Application {
     public static void main(String[] args) {
+
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.add(40);
+        bst.add(50);
+        bst.add(30);
+        bst.add(20);
+        bst.add(35);
+        bst.add(32);
+
+        bst.print();
+//        doubleLinkedList();
+//        singleLinkedList();
+    }
+
+    private static void doubleLinkedList() {
         DoubleLinkedNode node1 = new DoubleLinkedNode(20);
         DoubleLinkedNode node2 = new DoubleLinkedNode(40);
         DoubleLinkedNode node3 = new DoubleLinkedNode(50);
         DoubleLinkedNode node4 = new DoubleLinkedNode(30);
 
         DoubleLinkedList list = new DoubleLinkedList();
-//        list.add(node1);
-//        list.add(node2);
+        list.add(node1);
+        list.add(node2);
         list.add(node3);
-//        list.add(node4);
+        list.add(node4);
 
-//        System.out.println("Elements: " + list.print());
-//        System.out.println("Elements: " + list.printReverse());
-//        list.remove(40);
-//        System.out.println("Elements: " + list.print());
-//        System.out.println("Elements: " + list.printReverse());
-//        list.remove(30);
-//        System.out.println("Elements: " + list.print());
-//        System.out.println("Elements: " + list.printReverse());
-//        list.remove(20);
-//        System.out.println("Elements: " + list.print());
-//        System.out.println("Elements: " + list.printReverse());
+        System.out.println("Elements: " + list.print());
+        System.out.println("Elements: " + list.printReverse());
+        list.remove(40);
+        System.out.println("Elements: " + list.print());
+        System.out.println("Elements: " + list.printReverse());
+        list.remove(30);
+        System.out.println("Elements: " + list.print());
+        System.out.println("Elements: " + list.printReverse());
+        list.remove(20);
+        System.out.println("Elements: " + list.print());
+        System.out.println("Elements: " + list.printReverse());
         list.remove(50);
         System.out.println("Elements: " + list.print());
         System.out.println("Elements: " + list.printReverse());
 
-
-        // singleLinkedList();
+        // If we add the existing node4 we will get all the previous links of that node
+        // Multiple ways to fix it:
+        // * keep all the nodes inside the class LinkedList and add ints
+        // * always send new nodes
+        // * always set the links of the removed node to null
+        list.add(new DoubleLinkedNode(30));
+        System.out.println("Elements: " + list.print());
+        System.out.println("Elements: " + list.printReverse());
     }
 
     private static void singleLinkedList() {
